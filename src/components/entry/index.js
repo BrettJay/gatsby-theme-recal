c37084f0a18bd115085257075a0ba1d9c2efee89
Brett Jones <hello@brettjones.me>
2020-01-08 19:48:21 +0800

    Update entry page for when no entries are found

--- src/components/entry/index.js
+++ src/components/entry/index.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
+import { jsx, Styled } from 'theme-ui'
 import { format, parse } from 'date-fns'
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
@@ -7,9 +7,7 @@ import Image from "./image"
 import EntryLocations from "./locations"
 import EntryWeather from "./weather"
 
-function Entry(props) {
-  const { day } = props
-
+function EntryContainer(props) {
   return (
     <div
       sx={{
@@ -18,28 +16,74 @@ function Entry(props) {
         'WebkitOverflowScrolling': 'touch',
         position: 'relative'
       }}>
-      <div
-        sx={{
-          textAlign: `center`,
-          lineHeight: `50px`,
-          borderBottomWidth: `1px`,
-          borderBottomStyle: `solid`,
-          borderBottomColor: `divider`,
-          position: `sticky`,
-          top: `0`,
-          background: `#fff`,
-          color: `primary`,
-          zIndex: `1`
-        }}>
+      {props.children}
+    </div>
+  )
+}
+
+function EntryHeader(props) {
+  return (
+    <div
+      sx={{
+        textAlign: `center`,
+        lineHeight: `50px`,
+        borderBottomWidth: `1px`,
+        borderBottomStyle: `solid`,
+        borderBottomColor: `divider`,
+        position: `sticky`,
+        top: `0`,
+        background: `#fff`,
+        color: `primary`,
+        zIndex: `1`
+      }}>
+      {props.children}
+    </div>
+  )
+}
+
+function EntryBody(props) {
+  return (
+    <div
+      sx={{
+        maxWidth: `800px`,
+        m: `0 auto`,
+        p: [`20px 16px 40px`, `40px 20px 40px`, `40px 80px 80px`],
+      }}>
+      {props.children}
+    </div>
+  )
+}
+
+function Entry(props) {
+  const { day } = props
+  if (!day) {
+    return (
+      <EntryContainer>
+        <EntryHeader>No entries found</EntryHeader>
+        <EntryBody>
+          <Styled.h2
+            sx={{
+              textAlign: `center`,
+            }}>
+            No log entries found
+          </Styled.h2>
+          <Styled.p
+            sx={{
+              textAlign: `center`,
+            }}>
+            Check the <span sx={{ p: `3px 6px`, color: 'tertiary', background: '#f7f9fa', display: `inline-block`, borderRadius: `6px`}}>/entries/</span> directory.
+          </Styled.p>
+        </EntryBody>
+      </EntryContainer>
+    )
+  }
+  return (
+    <EntryContainer>
+      <EntryHeader>
         {format(parse(day.frontmatter.date, 'yyyy/MM/dd', new Date()), 'EEEE d MMM, yyyy')}
-      </div>
+      </EntryHeader>
       <Image date={day.frontmatter.date}/>
-      <div
-        sx={{
-          maxWidth: `800px`,
-          m: `0 auto`,
-          p: [`20px 16px 40px`, `40px 20px 40px`, `40px 80px 80px`],
-        }}>
+      <EntryBody>
         <MDXRenderer>
           {day.body}
         </MDXRenderer>
@@ -47,8 +91,8 @@ function Entry(props) {
           {day.frontmatter.locations.length && <EntryLocations day={day}/>}
           <EntryWeather day={day}/>
         </div>
-      </div>
-    </div>
+      </EntryBody>
+    </EntryContainer>
   )
 }
 
